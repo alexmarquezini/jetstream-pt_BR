d6bf6581bb89af5588652b3866a2b9c0ed9a6e6e
Alex  Marquezini <mail@alexmarquezini.com>
2022-02-22 20:54:02 -0300

    Update auth.php

--- resources/lang/pt_BR/auth.php
+++ resources/lang/pt_BR/auth.php
@@ -13,5 +13,6 @@ return [
     */
 
     'failed'   => 'Credenciais informadas não correspondem com nossos registros.',
+    'password' => 'A senha fornecida está incorreta.',
     'throttle' => 'Você realizou muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
 ];
